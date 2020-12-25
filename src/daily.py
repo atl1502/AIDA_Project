1862d221476f2f50679fb5a8512214ac088b81a4
Andrew <atl15c02@gmail.com>
2020-12-24 20:10:38 -0600

    align DF indexs

--- src/daily.py
+++ src/daily.py
@@ -16,22 +16,22 @@ def get_monthly_averages(df):
     # March
     avg_march = df.loc[f'{year}-03-01' : f'{year}-03-31'].mean()
     if not np.isnan(avg_march):
-      output[f'{year}-03'] = avg_march
+      output[f'{year}-03-31'] = avg_march
 
     # June
     avg_june = df.loc[f'{year}-06-01' : f'{year}-06-30'].mean()
     if not np.isnan(avg_june):
-      output[f'{year}-06'] = avg_june
+      output[f'{year}-06-30'] = avg_june
 
     # September
     avg_september = df.loc[f'{year}-09-01' : f'{year}-09-30'].mean()
     if not np.isnan(avg_september):
-      output[f'{year}-09'] = avg_september
+      output[f'{year}-09-30'] = avg_september
 
     # December
     avg_december = df.loc[f'{year}-12-01' : f'{year}-12-31'].mean()
     if not np.isnan(avg_december):
-      output[f'{year}-12'] = avg_december
+      output[f'{year}-12-31'] = avg_december
 
   return pd.DataFrame.from_dict(output, orient='index', columns=["Monthly Average"])
 
@@ -41,7 +41,7 @@ def write_output(df, ticker):
 
   if not os.path.exists(f'data/{ticker}'):
     os.makedirs(f'data/{ticker}')
-  
+
   output.to_csv(f'data/{ticker}/output.csv')
 
 df_prices = service.get_share_prices()
